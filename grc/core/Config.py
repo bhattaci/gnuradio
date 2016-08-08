2b861ea6cbbc0502d652d3284f35eae2051f50e7
Sebastian Koslowski <koslowski@kit.edu>
2016-08-08 20:58:09 +0200

    grc: yaml: load yaml wrapper (with Cheetah for now)

--- grc/core/Config.py
+++ grc/core/Config.py
@@ -33,10 +33,15 @@ class Config(object):
 
     hier_block_lib_dir = os.environ.get('GRC_HIER_PATH', Constants.DEFAULT_HIER_BLOCK_LIB_DIR)
 
+    yml_block_cache = os.path.expanduser('~/.cache/grc_gnuradio')
+
     def __init__(self, prefs_file, version, version_parts=None):
         self.prefs = prefs_file
         self.version = version
         self.version_parts = version_parts or version[1:].split('-', 1)[0].split('.')[:3]
+
+        if not os.path.exists(self.yml_block_cache):
+            os.mkdir(self.yml_block_cache)
 
     @property
     def block_paths(self):
@@ -45,6 +50,7 @@ class Config(object):
         paths_sources = (
             self.hier_block_lib_dir,
             os.environ.get('GRC_BLOCKS_PATH', ''),
+            self.yml_block_cache,
             self.prefs.get_string('grc', 'local_blocks_path', ''),
             self.prefs.get_string('grc', 'global_blocks_path', ''),
         )
